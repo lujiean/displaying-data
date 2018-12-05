eec81d98aca4f16136852d1a3cae09459d2443b1
Lu <jiean.a.lu@accenture.com>
2018-12-05 17:23:23 +0800

    20181205 FUNDAMENTALS C&T Display Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite hero is: {{myHero}}</h2>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'displaying-data';
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
 }
